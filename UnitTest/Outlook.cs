f34525ee8569b8b628268002e90e6569d824f023
TroelsMA <Markvard24@hotmail.com>
2017-08-23 11:36:10 +0200

    UnitTest added

---
+++ UnitTest/Outlook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace UnitTest
+{
+    public class Outlook
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            bool value = true;
+
+            Assert.Equal(true, value);
+        }
+    }
+}
--- UnitTest/Properties/AssemblyInfo.cs
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OutlookShared")]
+[assembly: AssemblyTitle("UnitTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OutlookShared")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("56d77dcf-3181-4ee0-9b10-c16dfba37106")]
+[assembly: Guid("9abbd6a9-5f95-442a-88db-3fc1ebf374a7")]
 
 // Version information for an assembly consists of the following four values:
 //
