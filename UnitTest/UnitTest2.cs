fed7cea7c09732ce18e8d98a62c9e778045e5144
TroelsMA <Markvard24@hotmail.com>
2017-10-12 13:38:10 +0200

    Travis test

---
+++ UnitTest/UnitTest2.cs
@@ -0,0 +1,47 @@
+﻿using eFormShared;
+using OutlookSql;
+using OutlookCore;
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using Xunit;
+using System.Threading;
+using OutlookOffice;
+
+namespace UnitTest
+{
+    [Collection("Database collection")]
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test000_Basics_0a_EnvironmentMachineName()
+        {
+            //Arrange
+            string checkValueA = "";
+            string checkValueB = "Not correct";
+
+            //Act
+            checkValueB = Environment.MachineName;
+
+            //Assert
+            Assert.Equal(checkValueA, checkValueB);
+        }
+
+        [Fact]
+        public void Test000_Basics_1a_MustAlwaysPass()
+        {
+            //Arrange
+            bool checkValueA = true;
+            bool checkValueB = false;
+
+            //Act
+            checkValueB = true;
+
+            //Assert
+            Assert.Equal(checkValueA, checkValueB);
+        }
+    }
+}
