9afff902501e59631d8e69f02c138517fe10e6de
René Schultz Madsen <rm@microting.com>
2017-12-29 15:26:48 +0100

    Updated packages

---
+++ MicrotingOutlook.Integration.Tests/AppointmentTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using OutlookSql;
+
+namespace MicrotingOutlook.Integration.Tests
+{
+    [TestFixture]
+    public class AppointmentTests : DbTestFixture
+    {
+        [Test]
+        public void CanPeepTwice()
+        {
+            var appointment = new appointments();
+            DbContext.appointments.Add(appointment);
+
+            DbContext.SaveChanges();
+        }
+    }
+}
---
+++ MicrotingOutlook.Integration.Tests/DbTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OutlookSql;
+
+namespace MicrotingOutlook.Integration.Tests
+{
+    [TestFixture]
+    public abstract class DbTestFixture
+    {
+        protected OutlookDbMs DbContext;
+        protected string ConnectionString => @"data source=(LocalDb)\SharedInstance;Initial catalog=eformoutlook-tests";
+
+        [SetUp]
+        public void Setup()
+        {
+            DbContext = new OutlookDbMs(ConnectionString);
+            DbContext.Database.CreateIfNotExists();
+
+            DbContext.Database.Initialize(false);
+
+            DoSetup();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            DbContext.Database.Delete();
+            DbContext.Dispose();
+        }
+
+        public virtual void DoSetup() { }
+    }
+}
